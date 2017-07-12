1d058f3f8f244dbd7def9e02209023e35eadddb7
Sergey Grankin <sgrankin@iown1.net>
2017-07-12 16:26:04 -0400

    govendor -> go dep

--- vendor/github.com/Shopify/sarama/consumer.go
+++ vendor/github.com/Shopify/sarama/consumer.go
@@ -10,12 +10,11 @@ import (
 
 // ConsumerMessage encapsulates a Kafka message returned by the consumer.
 type ConsumerMessage struct {
-	Key, Value     []byte
-	Topic          string
-	Partition      int32
-	Offset         int64
-	Timestamp      time.Time // only set if kafka is version 0.10+, inner message timestamp
-	BlockTimestamp time.Time // only set if kafka is version 0.10+, outer (compressed) block timestamp
+	Key, Value []byte
+	Topic      string
+	Partition  int32
+	Offset     int64
+	Timestamp  time.Time // only set if kafka is version 0.10+
 }
 
 // ConsumerError is what is provided to the user when an error occurs.
@@ -521,13 +520,12 @@ func (child *partitionConsumer) parseResponse(response *FetchResponse) ([]*Consu
 
 			if offset >= child.offset {
 				messages = append(messages, &ConsumerMessage{
-					Topic:          child.topic,
-					Partition:      child.partition,
-					Key:            msg.Msg.Key,
-					Value:          msg.Msg.Value,
-					Offset:         offset,
-					Timestamp:      msg.Msg.Timestamp,
-					BlockTimestamp: msgBlock.Msg.Timestamp,
+					Topic:     child.topic,
+					Partition: child.partition,
+					Key:       msg.Msg.Key,
+					Value:     msg.Msg.Value,
+					Offset:    offset,
+					Timestamp: msg.Msg.Timestamp,
 				})
 				child.offset = offset + 1
 			} else {
@@ -727,10 +725,6 @@ func (bc *brokerConsumer) fetchNewMessages() (*FetchResponse, error) {
 	}
 	if bc.consumer.conf.Version.IsAtLeast(V0_10_0_0) {
 		request.Version = 2
-	}
-	if bc.consumer.conf.Version.IsAtLeast(V0_10_1_0) {
-		request.Version = 3
-		request.MaxBytes = MaxResponseSize
 	}
 
 	for child := range bc.subscriptions {
